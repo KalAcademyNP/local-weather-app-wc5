552f0a38a2d488f38c7a783fc24c3aab8c56b6cd
Kal <info@kalacademy.org>
2019-08-26 16:26:12 -0700

    Angular tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { CitySearchComponent } from './city-search/city-search.component';
+import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { WeatherService } from './weather/weather.service';
+import { WeatherServiceFake } from './weather/weather.service.fake';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        FormsModule,
+        ReactiveFormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        CurrentWeatherComponent,
+        CitySearchComponent
       ],
+      providers: [{provide: WeatherService, useClass: WeatherServiceFake}],
     }).compileComponents();
   }));
 
@@ -26,10 +47,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('local-weather-app');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to local-weather-app!');
-  });
+ 
 });
